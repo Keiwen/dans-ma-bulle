479c59485d50c2e78c82a0a705fd1dd57c49e6f6
keiwen <keiwen.mk@gmail.com>
2025-12-15 20:34:53 +0100

    feat: DMB-13 Adjust style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,14 +11,14 @@ const theme = computed(() => store.getters.theme)
 
 <template>
   <nav :class="theme === 'light' ? 'menu-light' : 'menu-dark'">
+    <div class="nav-item" aria-label="Navigate to settings">
+      <router-link to="/settings">Settings</router-link>
+    </div>
     <div class="nav-item">
       <router-link to="/book" aria-label="Navigate to book selection">Book</router-link>
     </div>
     <div class="nav-item" aria-label="Navigate to read page">
       <router-link to="/">Read</router-link>
-    </div>
-    <div class="nav-item" aria-label="Navigate to settings">
-      <router-link to="/settings">Settings</router-link>
     </div>
   </nav>
 </template>
